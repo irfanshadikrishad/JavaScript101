83be16d3baf730f6d571279e55a69d144ab0f9c2
Irfan Shadik <77362427+irfanshadikofficial@users.noreply.github.com>
2022-09-10 21:23:25 +0600

    Add files via upload

---
+++ Day 1 - Counter APP/script.js
@@ -0,0 +1,9 @@
+function add(){
+    document.getElementById('count').innerHTML ++;
+}
+function reset(){
+    document.getElementById('count').innerHTML = 0;
+}
+function sub(){
+    document.getElementById('count').innerHTML --;
+}
