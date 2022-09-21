70080368db522dd7cec5d66bd19d0bcda7ad7a0e
Irfan Shadik <77362427+irfanshadikofficial@users.noreply.github.com>
2022-09-21 23:05:08 +0600

    21 09 2022

---
+++ day75-websitepreloader/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', ()=> {
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('postloader').style.display = 'inline-block';
+})
