8aca3a835d5f260b918a4c78bcf4a7cc730183a0
Irfan Shadik <77362427+irfanshadikofficial@users.noreply.github.com>
2022-10-04 01:53:15 +0600

    Add files via upload

---
+++ day68-typewritingeffectproject/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing", {
+    strings:[
+        "Web Developer", 
+        "Web Designer", 
+        "Youtuber", 
+        "Blogger", 
+        "Programmer"],
+    typeSpeed:80,
+    smartBackspace:true,
+    shuffle:false,
+    loop:true,
+    loopCount:Infinity,
+    showCursor:true,
+    cursorChar: '|',
+    backSpeed:80,})
